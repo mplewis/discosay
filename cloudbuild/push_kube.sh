a3169b5580bd4cb7480c3683481d5d5b44c646c2
Matt Lewis <matt@mplewis.com>
2019-09-15 13:52:28 -0700

    fix: use short sha everywhere

--- cloudbuild/push_kube.sh
+++ cloudbuild/push_kube.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-set -euxo pipefail
+set -euo pipefail
 IFS=$'\n\t'
 
 cd retf-env
+git checkout candidate
 git add kubernetes.yaml
-git commit -m "Deploying from retf-app @ ${COMMIT_SHA}"
+git commit -m "Deploying from retf-app @ ${SHORT_SHA}"
 git push origin candidate
