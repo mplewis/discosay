644ee2ea333c4b9d287a29f6ef918714ad9eb40a
Matt Lewis <matt@mplewis.com>
2019-09-15 21:39:49 -0700

    fix: output to workspace

--- cloudbuild/deploy.sh
+++ cloudbuild/deploy.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 IFS=$'\n\t'
 
 INPUT_FILE="cloudbuild/kubernetes.tpl.yaml"
-OUTPUT_FILE="/tmp/kubernetes.yaml"
+OUTPUT_FILE="kubernetes.yaml"
 IMAGE="gcr.io/$PROJECT_ID/retf:$SHORT_SHA"
 
 sed "s!CONTAINER_IMAGE!$IMAGE!g" "$INPUT_FILE" > "$OUTPUT_FILE"
