67cac2ce09dba0d47ceb8cb8127cc3a5de7a9cb2
Matt Lewis <matt@mplewis.com>
2019-10-24 21:37:31 -0600

    jq raw

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,6 +10,6 @@ curl -Lo jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
 chmod a+x jq
 
 export IMAGE
-IMAGE="$(./jq '.builds[0].tag' < skaffold_output.json)"
+IMAGE="$(./jq '.builds[0].tag' -r < skaffold_output.json)"
 sed "s/CONTAINER_IMAGE/$IMAGE/" < "$TEMPLATE" > retf.yaml
 cat retf.yaml
