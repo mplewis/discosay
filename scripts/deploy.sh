3e7f5f20aa8b3adcf48abb22500c6b2cc4276938
Matt Lewis <matt@mplewis.com>
2019-10-24 20:23:29 -0600

    feat: deploy partial

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 set -euo pipefail
 IFS=$'\n\t'
+
+export TEMPLATE="$HOME/repo/kubernetes.tpl.yaml"
+
+cd ~
+
+curl -Lo jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
+chmod a+x jq
+
+export IMAGE
+IMAGE="$(./jq '.builds[0].tag' < skaffold_output.json)"
+sed "s/CONTAINER_IMAGE/$IMAGE/" < "$TEMPLATE" > retf.yaml
+cat retf.yaml
