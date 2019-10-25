0ea5ca7b137da52d424a3b47b8e70f4eafefe7fe
Matt Lewis <matt@mplewis.com>
2019-10-24 21:19:42 -0600

    fix: home

--- scripts/build_and_push.sh
+++ scripts/build_and_push.sh
@@ -6,7 +6,7 @@ cd ~
 
 echo "$GCP_TOKEN" > gcp_token.json
 export GOOGLE_APPLICATION_CREDENTIALS="$HOME/gcp_token.json"
-gcloud auth activate-service-account --key-file=~/gcp_token.json
+gcloud auth activate-service-account --key-file="$HOME/gcp_token.json"
 gcloud --quiet config set project "$GCP_PROJECT"
 gcloud --quiet config set compute/zone "$GCP_ZONE"
 
