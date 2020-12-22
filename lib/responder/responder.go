a56a54aa3e8b283c0543665cfc72f49bfa69e8be
Matt Lewis <matt@mplewis.com>
2020-12-21 20:10:02 -0700

    wip: parse responder from yaml blob

--- lib/responder/responder.go
+++ lib/responder/responder.go
@@ -8,8 +8,8 @@ import (
 )
 
 type Responder struct {
-	Name  string        // The name of the responder
-	Match regexp.Regexp // Only respond to messages that match this regex
+	Name  string         // The name of the responder
+	Match *regexp.Regexp // Only respond to messages that match this regex
 
 	// Optional. A list of responses, from which one is randomly selected
 	Responses []string
@@ -20,6 +20,41 @@ type Responder struct {
 	// Optional. If provided, the probability from 0.0 to 1.0 that this
 	// responder will fire on any given matched message
 	Probability float32
+}
+
+func Parse(name string, from map[string]interface{}) (*Responder, error) {
+	re, err := regexp.Compile(from["match"].(string))
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println(from["responses"])
+	responses := []string{}
+	rawResps, found := from["responses"]
+	if found {
+		for _, resp := range rawResps.([]interface{}) {
+			responses = append(responses, resp.(string))
+		}
+	}
+	log.Println(responses)
+
+	probability, found := from["probability"].(float32)
+	if !found {
+		probability = 0.0
+	}
+
+	template, found := from["template"].(string)
+	if !found {
+		template = ""
+	}
+
+	return &Responder{
+		Name:        name,
+		Match:       re,
+		Responses:   responses,
+		Template:    template,
+		Probability: probability,
+	}, err
 }
 
 func (r *Responder) roll() bool {
@@ -41,8 +76,11 @@ func (r *Responder) match(msg string) (bool, string) {
 }
 
 func (r *Responder) response() string {
-	n := rand.Intn(len(r.Responses))
-	return r.Responses[n]
+	rc := len(r.Responses)
+	if rc == 0 {
+		return ""
+	}
+	return r.Responses[rand.Intn(rc)]
 }
 
 func (r *Responder) Respond(in string) *string {
@@ -55,6 +93,10 @@ func (r *Responder) Respond(in string) *string {
 	if msg == "" {
 		log.Println("no captured msg")
 		msg = r.response()
+	}
+	if msg == "" {
+		log.Println("no response msg")
+		return nil
 	}
 	if r.Template != "" {
 		log.Println("using template")
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"os"
 	"time"
+
+	"github.com/mplewis/discosay/lib/responder"
+	"gopkg.in/yaml.v3"
 )
 
 func env(key string) string {
@@ -17,17 +21,18 @@ func env(key string) string {
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
-	// configPath := env("CONFIG_PATH")
-	// rawYaml, err := ioutil.ReadFile(configPath)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// config := make(map[string]interface{})
-	// err = yaml.Unmarshal(rawYaml, config)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// log.Printf("%#v\n", config)
+
+	configPath := env("CONFIG_PATH")
+	rawYaml, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	config := make(map[string]interface{})
+	err = yaml.Unmarshal(rawYaml, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("%#v\n", config)
 
 	// templates := config["templates"]
 	// log.Printf("%#v\n", templates)
@@ -35,8 +40,43 @@ func main() {
 	// bots := config["bots"]
 	// log.Printf("%#v\n", bots)
 
-	// responders := config["responders"]
+	responders := config["responders"].(map[string]interface{})
 	// log.Printf("%#v\n", responders)
+
+	for name, rspec := range responders {
+		spec := rspec.(map[string]interface{})
+		log.Printf("%#v: %#v\n", name, spec)
+		r, err := responder.Parse(name, spec)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("%#v\n", r)
+
+		response := r.Respond("!riir")
+		if response != nil {
+			log.Printf("-> %s: %s", r.Name, *response)
+		}
+
+		response = r.Respond("!retf")
+		if response != nil {
+			log.Printf("-> %s: %s", r.Name, *response)
+		}
+
+		response = r.Respond("!retf Rust rules!")
+		if response != nil {
+			log.Printf("-> %s: %s", r.Name, *response)
+		}
+
+		response = r.Respond("!gotime")
+		if response != nil {
+			log.Printf("-> %s: %s", r.Name, *response)
+		}
+
+		response = r.Respond("!gotime yay for go!")
+		if response != nil {
+			log.Printf("-> %s: %s", r.Name, *response)
+		}
+	}
 
 	// x, err := regexp.Compile("^!cheer$")
 	// if err != nil {
